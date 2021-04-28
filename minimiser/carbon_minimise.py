638c0c08beb0a3436760ed33d726152a18b124b1
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-28 16:26:45 +0100

    minimiser: added get optimal time

--- minimiser/carbon_minimise.py
+++ minimiser/carbon_minimise.py
@@ -18,8 +18,11 @@ class Minimiser:
         sorted_options = sorted(options, key=options.get)
         return sorted_options[0]
 
-    def optimal_time_for_location(self, location, num_options=1):
-        return True
+    async def optimal_time_for_location(self, location, num_options=1):
+        times = await self.api.region_forecast_range(location_ids[location], 48)
+        sorted_times = sorted(times, key=lambda x: x['forecast'])
+        optimal_times = [time['time'] for time in sorted_times[0:num_options]]
+        return optimal_times
 
     def optimal_time_and_location(self, locations):
         return True
