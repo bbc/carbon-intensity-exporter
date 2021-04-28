407ff76d1b659c164b080c7074960735feb8b987
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-28 17:42:04 +0100

    minimiser: change to use location strings not ids

--- minimiser/carbon_minimise.py
+++ minimiser/carbon_minimise.py
@@ -1,10 +1,5 @@
 from api.carbon import CarbonAPI
 
-location_ids = {
-    "LONDON": 13,
-    "NW_ENGLAND": 3
-}
-
 
 class Minimiser:
     def __init__(self):
@@ -13,13 +8,13 @@ class Minimiser:
     async def optimal_location_now(self, locations):
         options = {}
         for location in locations:
-            intensity, _ = await self.api.current_region_intensity(location_ids[location])
+            intensity, _ = await self.api.current_region_intensity(location)
             options[location] = intensity
         sorted_options = sorted(options, key=options.get)
         return sorted_options[0]
 
     async def optimal_time_for_location(self, location, num_options=1):
-        times = await self.api.region_forecast_range(location_ids[location], 48)
+        times = await self.api.region_forecast_range(location, 48)
         sorted_times = sorted(times, key=lambda x: x['forecast'])
         optimal_times = [time['time'] for time in sorted_times[0:num_options]]
         return optimal_times
@@ -27,7 +22,7 @@ class Minimiser:
     async def optimal_time_and_location(self, locations, num_options=1):
         options = []
         for location in locations:
-            times = await self.api.region_forecast_range(location_ids[location], 48)
+            times = await self.api.region_forecast_range(location, 48)
             for time in times:
                 # results don't come back with location attached
                 time['location'] = location
