548442dd703f4330b16e48af0feef6c1a8d61760
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-28 16:58:51 +0100

    api: replace time conversion function with call to datetime iso function

--- api/carbon.py
+++ api/carbon.py
@@ -6,13 +6,6 @@ class CarbonAPI:
     def __init__(self):
         self.api = ApiConnection("https://api.carbonintensity.org.uk/")
 
-    @staticmethod
-    def _get_api_time_format():
-        time_now = datetime.utcnow()
-        # reformat to yyyy-mm-ddThh:mm:ss+0000 to fit API call
-        time_now = str(time_now).replace(" ", 'T')
-        return time_now[:-7] + "+0000"
-
     """
     Returns a tuple of the combined average intensity for England, Scotland, and Wales
     along with a descriptor of intensity from very low -> very high
@@ -55,7 +48,7 @@ class CarbonAPI:
     down to the nearest half hour)
     """
     async def national_forecast_single(self, hours):
-        json = await self.api.get(f"intensity/{self._get_api_time_format()}/fw48h")
+        json = await self.api.get(f"intensity/{datetime.utcnow().isoformat()}/fw48h")
         # endpoint returns half hourly predictions from current half hour rounded
         # so index 2n gives n hours from now. Max time is therefore 47.5 hours
         index = int(hours*2) if hours < 48 else 95
@@ -68,7 +61,7 @@ class CarbonAPI:
     to the nearest half hour) and that many hours from now 
     """
     async def national_forecast_range(self, hours):
-        json = await self.api.get(f"intensity/{self._get_api_time_format()}/fw48h")
+        json = await self.api.get(f"intensity/{datetime.utcnow().isoformat()}/fw48h")
         # endpoint returns half hourly predictions from current half hour rounded
         index = int(hours*2) if hours < 48 else 95
         forecasts = json['data'][0: index]
@@ -86,7 +79,7 @@ class CarbonAPI:
     down to the nearest half hour)
     """
     async def region_forecast_single(self, region_id, hours):
-        json = await self.api.get(f"regional/intensity/{self._get_api_time_format()}/fw48h/regionid/{region_id}")
+        json = await self.api.get(f"regional/intensity/{datetime.utcnow().isoformat()}/fw48h/regionid/{region_id}")
         index = int(hours*2) if hours < 48 else 95
         prediction = json['data']['data'][index]['intensity']
         return prediction['forecast'], prediction['index']
@@ -98,7 +91,7 @@ class CarbonAPI:
     to the nearest half hour) and that many hours from now
     """
     async def region_forecast_range(self, region_id, hours):
-        json = await self.api.get(f"regional/intensity/{self._get_api_time_format()}/fw48h/regionid/{region_id}")
+        json = await self.api.get(f"regional/intensity/{datetime.utcnow().isoformat()}/fw48h/regionid/{region_id}")
         index = int(hours*2) if hours < 48 else 95
         forecasts = json['data']['data'][0: index]
         predictions = []
