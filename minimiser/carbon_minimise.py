d65cee560a154a279a051f2aad63e2546c5bec53
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-28 16:14:41 +0100

    minimiser: added get optimal location now

--- minimiser/carbon_minimise.py
+++ minimiser/carbon_minimise.py
@@ -1,12 +1,22 @@
 from api.carbon import CarbonAPI
 
+location_ids = {
+    "LONDON": 13,
+    "NW_ENGLAND": 3
+}
 
-class Minimiser():
+
+class Minimiser:
     def __init__(self):
         self.api = CarbonAPI()
 
-    def optimal_location_now(self):
-        return True
+    async def optimal_location_now(self, locations):
+        options = {}
+        for location in locations:
+            intensity, _ = await self.api.current_region_intensity(location_ids[location])
+            options[location] = intensity
+        sorted_options = sorted(options, key=options.get)
+        return sorted_options[0]
 
     def optimal_time_for_location(self, location, num_options=1):
         return True
