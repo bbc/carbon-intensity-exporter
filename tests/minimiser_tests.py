7bc8d4a8fc1fcca9739a7641968b8fa0de2126e8
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-29 12:05:16 +0100

    testing: added intial minimiser tests

---
+++ tests/minimiser_tests.py
@@ -0,0 +1,38 @@
+from asynctest import TestCase, mock
+from minimiser.carbon_minimise import Minimiser
+from api.carbon import CarbonAPI
+
+
+class TestCarbonMinimiser(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.min = Minimiser()
+
+    async def test_optimal_location_now(self):
+        pass
+
+    async def test_optimal_time_for_location(self):
+        data = [{'forecast': 231, 'index': 'moderate', 'time': '+00:30'},
+                {'forecast': 223, 'index': 'moderate', 'time': '+01:00'},
+                {'forecast': 218, 'index': 'moderate', 'time': '+01:30'}]
+        with mock.patch.object(CarbonAPI, "region_forecast_range", return_value=data):
+            result = await self.min.optimal_time_for_location("")
+            self.assertEqual(result, "+01:30")
+
+    async def test_optimal_time_and_location(self):
+        pass
+
+    async def test_optimal_time_window_for_location(self):
+        data = [{'forecast': 231, 'index': 'moderate', 'time': '+00:30'},
+                {'forecast': 223, 'index': 'moderate', 'time': '+01:00'},
+                {'forecast': 218, 'index': 'moderate', 'time': '+01:30'},
+                {'forecast': 100, 'index': 'low', 'time': '+02:00'},
+                {'forecast': 200, 'index': 'moderate', 'time': '+02:30'},
+                {'forecast': 218, 'index': 'moderate', 'time': '+03:00'}
+                ]
+        with mock.patch.object(CarbonAPI, "region_forecast_range", return_value=data):
+            result = await self.min.optimal_time_window_for_location("", 2)
+            self.assertEqual(result, "+02:00")
+
+    async def test_optimal_time_window_and_location(self):
+        pass
