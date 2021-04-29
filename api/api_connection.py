fc8ace727e0324e59c970961cb70c0bd0709cfc9
James Gibson <james.gibson@bbc.co.uk>
2021-04-29 12:30:56 +0100

    Use env variable for proxy

--- api/api_connection.py
+++ api/api_connection.py
@@ -7,14 +7,14 @@ class ApiConnection:
         self.base_url = base
 
     async def status(self):
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), trust_env=True) as session:
             async with session.get(self.base_url) as r:
                 return r.status
 
     async def get(self, endpoint):
         url = urljoin(self.base_url, endpoint)
         # issue with ssl certs overridden by ssl=false
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), trust_env=True) as session:
             async with session.get(url) as r:
                 json = await r.json()
                 return json
