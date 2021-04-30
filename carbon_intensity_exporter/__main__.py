852fb69f917a91c3aefc0133e750ba4fef60c655
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-04-30 16:12:27 +0100

    exporter: added -p flag to change port from default 8000

--- carbon_intensity_exporter/__main__.py
+++ carbon_intensity_exporter/__main__.py
@@ -2,13 +2,22 @@ from carbon_intensity_exporter.exporter.prometheus import Prometheus
 from prometheus_client import start_http_server
 import time
 from prometheus_client.core import REGISTRY
+import argparse
 
 
-def main():
-    start_http_server(8000)
+def main(port):
+    start_http_server(port)
     REGISTRY.register(Prometheus())
     while True:
         time.sleep(10)
 
 
-main()
+parser = argparse.ArgumentParser()
+parser.add_argument('-p')
+args = parser.parse_args()
+if args.p:
+    port = int(args.p)
+else:
+    port = 8000
+
+main(port)
