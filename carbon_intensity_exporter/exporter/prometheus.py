9a34d80e44057d6e0dc4ea181edb226d31e7fd53
James Gibson <james.gibson@bbc.co.uk>
2021-05-17 10:35:11 +0100

    Collect metrics from all regions of the UK

--- carbon_intensity_exporter/exporter/prometheus.py
+++ carbon_intensity_exporter/exporter/prometheus.py
@@ -1,5 +1,5 @@
 from prometheus_client.core import GaugeMetricFamily
-from carbon_intensity_exporter.carbon_api_wrapper.carbon import CarbonAPI
+from carbon_intensity_exporter.carbon_api_wrapper.carbon import CarbonAPI, REGIONS
 import asyncio
 from datetime import datetime
 
@@ -29,43 +29,39 @@ class Prometheus:
         r = loop.run_until_complete(t)
         return r
 
+    def collect_region(self, region):
+        """Collect metrics for a single region"""
+        api_calls = {
+            "int": self.api.current_region_intensity(region),
+            "mix": self.api.current_region_mix(region),
+            "forecast": self.api.region_forecast_range(region, 48),
+        }
+
+        results = {}
+
+        for metric, func in api_calls.items():
+            # prometheus doesn't support an async collect function, so run carbon_api_wrapper calls synchronously
+            results[metric] = self.execute_synchronously(func)
+
+        timestamp = str(datetime.now().timestamp())
+
+        self.gauges['intensity'].add_metric(labels=[region], timestamp=timestamp, value=results['int'][0])
+
+        for fuel, percent in results['mix'].items():
+            self.gauges['fuel_mix'].add_metric(labels=[region, fuel], timestamp=timestamp, value=percent)
+
+        for forecast in results['forecast']:
+            self.gauges['forecast'].add_metric(labels=[region,
+                                                forecast['time']],
+                                                timestamp=timestamp,
+                                                value=forecast['forecast'])
+
     def collect(self):
         healthy = self.execute_synchronously(self.api.health_status())
         self.gauges['up'].add_metric(labels=["Carbon API Collector"], value=healthy)
         if healthy:
-            api_calls = {
-                "lon_int": self.api.current_region_intensity("LONDON"),
-                "man_int": self.api.current_region_intensity("NW_ENGLAND"),
-                "lon_mix": self.api.current_region_mix("LONDON"),
-                "man_mix": self.api.current_region_mix("NW_ENGLAND"),
-                "lon_forecast": self.api.region_forecast_range("LONDON", 48),
-                "man_forecast": self.api.region_forecast_range("NW_ENGLAND", 48)
-            }
-            results = {}
-            for metric, func in api_calls.items():
-                # prometheus doesn't support an async collect function, so run carbon_api_wrapper calls syncronously
-                results[metric] = self.execute_synchronously(func)
-
-            timestamp = str(datetime.now().timestamp())
-
-            self.gauges['intensity'].add_metric(labels=["london"], timestamp=timestamp, value=results['lon_int'][0])
-            self.gauges['intensity'].add_metric(labels=["manchester"], timestamp=timestamp, value=results['man_int'][0])
-
-            for fuel, percent in results['lon_mix'].items():
-                self.gauges['fuel_mix'].add_metric(labels=["london", fuel], timestamp=timestamp, value=percent)
-            for fuel, percent in results['man_mix'].items():
-                self.gauges['fuel_mix'].add_metric(labels=["manchester", fuel], timestamp=timestamp, value=percent)
-
-            for forecast in results['lon_forecast']:
-                self.gauges['forecast'].add_metric(labels=["london",
-                                                   forecast['time']],
-                                                   timestamp=timestamp,
-                                                   value=forecast['forecast'])
-            for forecast in results['man_forecast']:
-                self.gauges['forecast'].add_metric(labels=["manchester",
-                                                   forecast['time']],
-                                                   timestamp=timestamp,
-                                                   value=forecast['forecast'])
+            for region in REGIONS.keys():
+                self.collect_region(region)
 
             for name, gauge in self.gauges.items():
                 yield gauge
